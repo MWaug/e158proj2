91bccc0c334c1c0b30261e96b4bcdd0e2b428d53
Jerry Hsiung <shsiung@g.hmc.edu>
2015-03-23 15:34:25 -0700

    modified controller.sv

--- verilog/controller.sv
+++ verilog/controller.sv
@@ -1,3 +1,11 @@
+// states and instructions
+
+typedef enum logic [1:0] {STATE_1 = 2'b00,
+							STATE_2 = 2'b01,
+							STATE_3 = 2'b10,
+							STATE_4 = 2'b11,
+							} statetype;
+
 module controller(input logic clk1, clk2, reset,
                   output logic dataClk1, dataClk2, clearAccum,
                   output logic[1:0] muxControl);
@@ -9,55 +17,67 @@ module controller(input logic clk1, clk2, reset,
 	parameter STATE_4 = 2'b11;
 
 	// State registers
-	reg [1:0] CurrentState ;
-	reg [1:0] NextState ;
+	statetype state;
 
-	// Register logic
-	always@ ( clk1 ) begin
-		if ( Reset ) CurrentState <= STATE_1;
-		else CurrentState <= NextState;
-	end
+	statelogic statelog(ph1, ph2, reset, state);
+	outputlogic outputlog(state, dataClk1, dataClk2, clearAccum, muxControl);
 
-	// Next state logic
-	always@ ( clk1 ) 
-	begin
-  		NextState = CurrentState ;
-	    case ( CurrentState )
-	 	    STATE_1 : NextState = STATE_2 ;
-		    STATE_2 : NextState = STATE_3 ;
-		    STATE_3 : NextState = STATE_4 ;
-		    STATE_4 : NextState = STATE_1 ;
-		default: next_state = START;
-	    endcase
-    end
+endmodule
 
+module statelogic(input  logic     ph1, ph2, reset,
+                  output statetype state);
+
+      statetype nextstate;
+  	  logic [1:0] ns, state_logic;
+
+  	  // 
+	  regr #(2) statereg(ph1, ph2, nextstate, state_logic);
+
+	  assign state = statetype'(state_logic);
+
+	  // Next state logic
+	  always_comb
+	    begin
+	      case (CurrentState)
+	        STATE_1 : nextstate = STATE_2 ;
+		    STATE_2 : nextstate = STATE_3 ;
+		    STATE_3 : nextstate = STATE_4 ;
+		    STATE_4 : nextstate = STATE_1 ;
+	        default: nextstate = STATE_1; // should never happen
+	      endcase
+	    end
+endmodule
+
+
+module outputlogic(input statetype state,
+                   output logic dataClk1, dataClk2, clearAccum,
+                   output logic[1:0] muxControl);
     // Output logic
-    always@ ( clk1 ) 
-    begin
-	    case ( CurrentState )
+    always_comb
+      begin
+	    case (state)
 	 	    STATE_1 : 
 	 	    begin 
-		    	muxControl <= STATE_1;
-		    	dataClk1 <= 0;
+		    	muxControl = 2b'00;
+		    	dataClk1 = 0;
 		    end
 		    STATE_2 : 
 	 	    begin 
-		    	muxControl <= STATE_2;
-		    	dataClk1 <= 0;
+		    	muxControl = 2b'01;
+		    	dataClk1 = 0;
 		    end
 		    STATE_3 : 
 	 	    begin 
-		    	muxControl <= STATE_3;
-		    	dataClk1 <= 0;
+		    	muxControl = 2b'10;
+		    	dataClk1 = 0;
 		    end
 		    STATE_4 : 
 	 	    begin 
-		    	muxControl <= STATE_4;
-		    	dataClk1 <= 1;
-		    	clearAccum <= 1;
+		    	muxControl = 2b'11;
+		    	dataClk1 = 1;
+		    	clearAccum = 1;
 		    end
-		default: muxControl <= STATE_1;
+		default: muxControl = 2b'00;
 	    endcase
-    end
-
+      end
 endmodule
